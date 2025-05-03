5993101f2990ab5cd3be48a6c6fade0ae7907ed2
PROGRAMISTO <habib2004habib@yandex.ru>
2025-05-03 15:17:46 +0300

    Add zone scripts

---
+++ Assets/# Scripts/Game/Environment/Zones/KillZone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class KillZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerController>().Kill();
+        }
+    }
+}
---
+++ Assets/# Scripts/Game/Environment/Zones/TriggerZone.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class TriggerZone : MonoBehaviour
+{
+    [Tooltip("Действие при входе в зону")]
+    public UnityEvent action;
+    [Tooltip("Действие при выходе из зоны")]
+    public UnityEvent actionOnExit;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            action.Invoke();
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            action.Invoke();
+        }
+    }
+}
