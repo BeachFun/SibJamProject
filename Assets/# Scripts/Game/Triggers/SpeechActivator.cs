cd2a118aa994b634ec2decae92fb88cde2c4a4ac
PROGRAMISTO <habib2004habib@yandex.ru>
2025-05-03 15:52:17 +0300

    Update SpeechActivator.cs

--- Assets/# Scripts/Game/Triggers/SpeechActivator.cs
+++ Assets/# Scripts/Game/Triggers/SpeechActivator.cs
@@ -1,17 +1,45 @@
 using UnityEngine;
+using UniRx;
 using Zenject;
 using RGames.Core;
+using System;
 
 public class SpeechActivator : MonoBehaviour, IActivatable
 {
     [SerializeField] private int dialogueID;
     [Inject] private SpeechManager dialogueManager;
+    [Inject] private InputService inputService;
+
+    private IDisposable interactionSubscription;
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && interactionSubscription == null)
+        {
+            interactionSubscription = inputService.Intaraction.Subscribe(OnInteract);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            interactionSubscription?.Dispose();
+        }
+    }
+
+
     public void Activate()
     {
         dialogueManager.ShowSpeech(dialogueID);
     }
-    public void OnTrigger()
+
+    private void OnInteract(bool keyState)
     {
-        Activate();
+        if (keyState)
+        {
+            Activate();
+        }
     }
 }
